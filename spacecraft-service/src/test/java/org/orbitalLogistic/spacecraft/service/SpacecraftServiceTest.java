a2bd958d0d85ae6bd267a829455dfb38b7c699c9
Владимир Данченко <368087@niuitmo.ru>
2025-12-11 01:54:50 +0300

    add spacecraft tests fix

--- spacecraft-service/src/test/java/org/orbitalLogistic/spacecraft/service/SpacecraftServiceTest.java
+++ spacecraft-service/src/test/java/org/orbitalLogistic/spacecraft/service/SpacecraftServiceTest.java
@@ -102,6 +102,8 @@ class SpacecraftServiceTest {
                 BigDecimal.ZERO,
                 BigDecimal.ZERO
         );
+
+        spacecraftService.setSpacecraftTypeService(spacecraftTypeService);
     }
 
     @Test
